3506e21e2faef8989c35fe6a08e173c8378849ba
Georg Konwisser <gekonwi@gmail.com>
2014-10-17 15:31:53 -0400

    #28: added the posessions pattern (JUnit test is missing)

--- Hadoop08/test/code/lemma/TokenizerTest.java
+++ Hadoop08/test/code/lemma/TokenizerTest.java
@@ -54,7 +54,6 @@ public class TokenizerTest {
 	@Test
 	public void testRegexDoesNotEndWithOr() {
 		String regex = Tokenizer.buildNoisePattern().pattern();
-		System.out.println(regex);
 		assertFalse(regex.charAt(regex.length() - 1) == "|".charAt(0));
 	}
 
