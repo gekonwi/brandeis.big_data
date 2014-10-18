1b03706544dd140f85f6f381151bc7e24d6138ea
Georg Konwisser <gekonwi@gmail.com>
2014-10-17 22:54:21 -0400

    #24: made readProfessions DRYer

--- Hadoop08/src/util/HDFSUtils.java
+++ Hadoop08/src/util/HDFSUtils.java
@@ -60,38 +60,20 @@ public class HDFSUtils {
 	public static HashMap<String, Integer> readProfessions(String filePath) throws IOException {
 		HashMap<String, Integer> result = new HashMap<String, Integer>();
 
-		Path professionsPath = new Path(filePath);
-		FileSystem fs = professionsPath.getFileSystem(new Configuration());
-		BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(professionsPath)));
-		// below is for testing purposes
-		// BufferedReader br = new BufferedReader(new FileReader(filePath));
-
-		String line;
-		while ((line = br.readLine()) != null) {
-			String professions = line.substring(line.indexOf(": ") + 2, line.length());
-			String[] professionsArr = professions.split(", ");
-			for (String s : professionsArr) {
-				if (result.containsKey(s))
-					result.put(s, result.get(s) + 1);
+		for (String line : readLines(filePath)) {
+			String[] professions = line.split(" : ")[1].split(", ");
+			for (String p : professions)
+				if (result.containsKey(p))
+					result.put(p, result.get(p) + 1);
 				else
-					result.put(s, 1);
-			}
+					result.put(p, 1);
 		}
-
-		br.close();
 
 		return result;
 	}
 
-	// below is for testing purposes
-//	public static void main(String[] args) throws IOException {
-//		HashMap<String, Integer> temp = readProfessions("Profession_test.txt");
-//		for (String s : temp.keySet()) {
-//			System.out.println(s + " " + temp.get(s));
-//		}
-//	}
-
-	// don't know if we need really, but here is a method for counting the lines of an input file
+	// don't know if we need really, but here is a method for counting the lines
+	// of an input file
 	// http://stackoverflow.com/questions/453018/number-of-lines-in-a-file-in-java
 	public static int countLines(String filePath) throws IOException {
 		Path professionsPath = new Path(filePath);
@@ -115,23 +97,24 @@ public class HDFSUtils {
 			is.close();
 		}
 	}
-	
+
 	/**
 	 * Write professions count list out to file, professions_count.txt
+	 * 
 	 * @param filePath
 	 * @throws IOException
 	 */
-	public static void writeProfessions(String filePath) throws IOException{
+	public static void writeProfessions(String filePath) throws IOException {
 		File file = new File("professions_count.txt");
-		if(!file.exists())
+		if (!file.exists())
 			file.createNewFile();
 		BufferedWriter bw = new BufferedWriter(new FileWriter(file));
-		
+
 		HashMap<String, Integer> professions = readProfessions(filePath);
-		
-		for(String s:professions.keySet())
+
+		for (String s : professions.keySet())
 			bw.write(s + " " + professions.get(s));
-		
+
 		bw.close();
 	}
 }
