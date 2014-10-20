1a0f5dfbb99d9f3ca2d9f211b80a515489af8d7f
Georg Konwisser <gekonwi@gmail.com>
2014-10-19 22:22:30 -0400

    #23: added test input files and calculated the expected output for a ProfessionClassifierMapred MapReduce JUnit test.

---
+++ Hadoop08/test/code/profession/ProfessionClassifierMapredTest.java
@@ -0,0 +1,20 @@
+package code.profession;
+
+public class ProfessionClassifierMapredTest {
+	/**
+	 * <pre>
+	 * TODO Let DIR = test_data/ProfessionClassifierMapredTest. Run classifier on:
+	 * DIR/article_index.txt as input, 
+	 * DIR/profession_index.txt as PROFESSION_INDEX, and 
+	 * DIR/wanted_people.txt as the list of people we care about.
+	 * 
+	 * The expected output is:
+	 * Will Smith : actor, doctor, soccer player
+	 * Bill Clinton : actor, doctor, soccer player
+	 * Mario Gömez : soccer player, soccer coach, doctor
+	 * Albert Einstein : scientist, teacher, actor
+	 * 
+	 * (see DIR/math.txt for details)
+	 * </pre>
+	 */
+}
