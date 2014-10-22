5016753b54cf910ac2cfdc3995faf80fa6fc2160
harkan94 <harkan@brandeis.edu>
2014-10-22 11:42:07 -0400

    Cleaned up Benchmark class and test files

--- Hadoop08/src/code/profession/Benchmark.java
+++ Hadoop08/src/code/profession/Benchmark.java
@@ -11,63 +11,63 @@ public class Benchmark {
 
 	public static void main (String[] args) throws IOException {
 		// Call split() method if running for first time
-		
-		BufferedReader in = new BufferedReader(new FileReader("test_profession.txt"));	 //result of classification on prof_train.test.
-		BufferedReader in2 = new BufferedReader(new FileReader("real_profession.txt"));  	 //solution to prof_train.test.
-		String line = in.readLine(); //line of result.
-		String line2 = in2.readLine(); //line of solution key.
+		BufferedReader in = new BufferedReader(new FileReader("test_profession.txt"));	//result of classification on prof_train.test.
+		BufferedReader in2 = new BufferedReader(new FileReader("real_profession.txt")); //solution to prof_train.test.
+		String line = in.readLine(); 							//line of result.
+		String lineSol = in2.readLine(); 						//line of solution key.
 		double correct = 0; 
 		double counter = 0;
 		while (line !=null) {
-			String profList = truncate(line, 1); //string of professions in result line.
+			String profList = truncate(line, 1); 					//string of professions in result line.
 			for (String guess : profList.split(", "))
-				   if(line2.contains(guess)) {
+				   if(lineSol.contains(guess)) {
 				      correct++;
 				      break;
 				   }
 			counter++;
 			line = in.readLine();
-			line2 = in2.readLine();
+			lineSol = in2.readLine();
 		}
 		double precision = correct/counter;
 		System.out.println("Your precision is " + precision);
 	}
 	
-	public static void split() throws IOException { //method used to split profession_train into *train.known, *train.test, and *train.sol
-		
+	public static void split() throws IOException { 
+		//method used to split profession_train into *train.known, *train.test, and *train.sol
 		BufferedReader in = new BufferedReader(new FileReader("profession_train.txt"));
-		BufferedWriter out = new BufferedWriter(new FileWriter("prof_train.known.txt"));
-		BufferedWriter out2 = new BufferedWriter(new FileWriter("prof_train.test.txt"));
-		BufferedWriter out3 = new BufferedWriter(new FileWriter("prof_train.sol.txt"));
+		BufferedWriter outKnown = new BufferedWriter(new FileWriter("prof_train.known.txt"));
+		BufferedWriter outTest = new BufferedWriter(new FileWriter("prof_train.test.txt"));
+		BufferedWriter outSol = new BufferedWriter(new FileWriter("prof_train.sol.txt"));
 		
 		
 		long counter = 0;
 		
 		String line = in.readLine();
-		while (line != null) {								//loop as long as there is more input to be read
+		while (line != null) {								//loop as long as there is more input 
 			if (counter < 600000) {							//first 600k reviews
-				out.write(line + "\n");;  					//write output into prof_train.known.txt
+				outKnown.write(line + "\n");;  					//write output into prof_train.known.txt
 				counter++;		
 			}
 			else {					
-				out3.write(line + "\n");					//write solution into prof_train.sol.txt
-				out2.write(truncate(line,0) + "\n");			//write output into prof_train.test.txt
+				outSol.write(line + "\n");					//write solution into prof_train.sol.txt
+				outTest.write(truncate(line,0) + "\n");				//write output into prof_train.test.txt
 			}
 			line = in.readLine();							//move on to next line
 		}
-		
-		//close the BufferedWriter and BufferedReader because we are done
+												//close the BufferedWriter and BufferedReader 
 				in.close();
-				out.close();
-				out2.close();
-				out3.close();
+				outKnown.close();
+				outTest.close();
+				outSol.close();
 	}
+
 	public static String truncate(String line, int test) {
-		String[] lineParts = line.split(" : ", 2);				//divides the line String into a String array of length 2.
+
+		String[] lineParts = line.split(" : ", 2);		//divides the line String into a String array of length 2.
 		if (test == 0) {
-			return lineParts[0];								//returns the String at index 0, which is the first half of truncation (the name).
-		} else {
-			return lineParts[1];
+			return lineParts[0];				//string at index 0 is the first half of truncation(the name) 
+		} else {						
+			return lineParts[1];				//string at index 1 is the seecond half(the professions)
 		}
 	}
 }
--- Hadoop08/test/code/profession/BenchmarkTest2.java
+++ Hadoop08/test/code/profession/BenchmarkTest2.java
@@ -1,22 +1,25 @@
 package code.benchmark;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
-import org.junit.Before;
-import org.junit.Test;
-public class BenchmarkTest {
+
+public class BenchmarkTest2 {
 	
-	@Test 
-	public void testSplit() {
+	public static void main(String[] args) throws IOException {
+		testSplit();
+		testBenchmark();
+		
+	}
+
+	public static void testSplit() {
 		//Tests to see if file splits correctly
 		BufferedReader in = new BufferedReader(new FileReader("real_profession"));		//original file
 		BufferedWriter outKnown = new BufferedWriter(new FileWriter("known_profession"));	//part of original file for classifier
@@ -41,7 +44,7 @@ public class BenchmarkTest {
 		outKnown.close();
 		outTest.close();
 		outSol.close();
-
+		
 		Scanner console = new Scanner(new File("real_profession.txt"));		//scans original file (solution)
 		Scanner console2 = new Scanner(new File("known_profession.txt"));	//scans known profession file (used for classifier)
 		Scanner console3 = new Scanner(new File("test_profession.txt"));	//scans test file (doesn't contain professions)
@@ -54,24 +57,44 @@ public class BenchmarkTest {
 		
 		for (int i = 0; i < counter; i++) {					//same here, manually split which lines are being 
 			lineReal = console.nextLine();					//compared with a for loop. 
-			lineKnown = console2.nextLine();				
+			lineKnown = console2.nextLine();
+			boolean compare;				
 			int equals = testReal.compareTo(Known);
-			assertEquals("The input and expected output should match: \r\n" + lineReal + "\r\n" + lineKnown + "\r\n",0,equals);
+			if (equals == 0) {
+				compare = true;
+			} else {
+				compare = false;
+			}
+			System.out.println("The input and expected output should match:");
+			System.out.println(lineReal);
+			System.out.println(lineKnown);
+			System.out.println(compare);
 		}
 		
 		for (int i = 0; i < 2; i++) {
 			lineReal = console.nextLine();					
 			lineTest = console3.nextLine();
 			lineSol = console4.nextLine();
-			int equals = testReal.compareTo(lineSol);			//lines from solution file should match original
+			boolean compare;				
+			int equals = testReal.compareTo(Known);				//lines from solution file should match original
+			if (equals == 0) {
+				compare = true;
+			} else {
+				compare = false;
+			}			
 			boolean contains = lineReal.contains(lineTest);			//lines from test should be a substring of original
-			assertEquals("The input and expected output should match: \r\n" + lineReal + "\r\n" + lineSol + "\r\n",0,equals);
-			assertTrue("The input should contain the expected output: \r\n" + lineReal + "\r\n" + lineTest + "\r\n",contains);
+			System.out.println("The input and expected first output should match:");
+			System.out.println(lineReal);
+			System.out.println(lineSol);
+			System.out.println(compare);
+			System.out.println("The input should contain the expected output:");
+			System.out.println(lineReal);
+			System.out.println(lineSol);
+			System.out.println(contains);
 		}
 	}
 	
-	@Test
-	public void testBenchmark() {
+	public static void testBenchmark() {
 		//tests to see if precision is correct.
 		BufferedReader in = new BufferedReader(new FileReader("test_profession.txt"));	 
 		BufferedReader in2 = new BufferedReader(new FileReader("sol_profession.txt")); 	 
@@ -95,14 +118,13 @@ public class BenchmarkTest {
 		double check = 5.0/7.0;							//manually calculated correct = 5, total = 7
 		assertEquals("The expected output should match the actual: \r\n", precision, check);
 		
-	}
-	
-	@Test
+	}	
+
 	public static String truncate(String line, int test) {
-		String[] lineParts = line.split(" : ", 2);				//divides the line into a String array of length 2.
+		String[] lineParts = line.split(" : ", 2);				//divides the line into a String array of length 2
 		if (test == 0) {
 			return lineParts[0];						//string at index 0 is the first half (the name)
-		} else {								
+		} else {	
 			return lineParts[1];						//string at index 1 is the second half (professions)
 		}
 	}
