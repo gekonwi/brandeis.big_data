0b9dc8739e65673881978d6d8b0f6f47201a78e1
Paul Kang <harkan@brandeis.edu>
2014-11-04 19:26:44 -0500

    #50: Created a Line to SequenceFile Mapper

---
+++ AmazonAnalysis/src/hadoop08/line_to_SequenceFile/LineToSequenceFileMR.java
@@ -0,0 +1,60 @@
+package hadoop08.line_to_SequenceFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+
+/**
+ * TODO
+ * @author Paul, 
+ */
+public class LineToSequenceFileMR {
+
+	public static class LineToSequenceFileMapper extends Mapper<LongWritable, Text, LongWritable, Text> {
+
+		/**
+		 * TODO
+		 */
+		@Override
+		public void map(LongWritable lineNum, Text review, Context context) throws IOException,
+				InterruptedException {
+			context.write(lineNum, review);
+		}
+	}
+
+
+	public static void main(String[] args) throws Exception {
+		Job job = Job.getInstance(new Configuration());
+		job.setOutputKeyClass(LongWritable.class);
+		job.setOutputValueClass(Text.class);
+
+		job.setMapperClass(LineToSequenceFileMapper.class);
+
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(SequenceFileOutputFormat.class);
+		
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		job.setJarByClass(LineToSequenceFileMR.class);
+
+		// so we don't have to specify the job name when starting job on cluster
+		job.getConfiguration().set("mapreduce.job.queuename", "hadoop08");
+
+		// execute the job with verbose prints
+		job.waitForCompletion(true);
+	}
+}
