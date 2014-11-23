0b612fbd7c312ff5220bfc967fdae4b692f820da
recrudescence <calvinmwang@gmail.com>
2014-11-23 03:50:53 -0500

    misc updates to comments

--- AmazonRecommendation/src/hadoop08/preprocess/RemoveUniques.java
+++ AmazonRecommendation/src/hadoop08/preprocess/RemoveUniques.java
@@ -8,15 +8,35 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ * Remove all lines where the first token is unique in respect to its neighbors.
+ * That is, would only remove "human freckle" in the following sequence:
+ * 
+ * zebra stripe zebra stripe human freckle giraffe spot giraffe spot giraffe
+ * spot
+ * 
+ * @author Calvin Wang
+ * 
+ */
 public class RemoveUniques {
 	public static void main(String[] args) throws IOException {
+		// use arguments for filename input
 		if (args.length != 2)
 			throw new IllegalArgumentException("need 2 parameters: inputPath outputPath");
 
 		seekAndOmit(new File(args[0]), new File(args[1]));
 	}
 
-	public static void seekAndOmit(File in, File out) throws IOException {
+	/**
+	 * main method to find and omit writing a unique-token line into new file.
+	 * 
+	 * @param in
+	 *            File to read from
+	 * @param out
+	 *            File to write to
+	 * @throws IOException
+	 */
+	private static void seekAndOmit(File in, File out) throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader(in));
 		BufferedWriter bw = new BufferedWriter(new FileWriter(out));
 
@@ -37,30 +57,30 @@ public class RemoveUniques {
 
 			prev = prevScan.next();
 			curr = currScan.next();
-			
+
 			// previous line is different than current line, and previous line
 			// was also different from its previous line
 			if (!prev.equals(curr)) {
 				if (diff) {
 					// previous line must be unique, so we don't write it out
-					System.out.println("skipped " + ++skipped);
+					if (++skipped % 1000000 == 0)
+						System.out.println("omitted " + skipped);
 				} else {
-					// previous line was the same as its previous, but just diff from current
+					// previous was same as its previous, but diff from current
 					diff = true;
 					// this means previous was the last of its type
 					bw.write(previous + "\r\n");
 					count++;
 				}
 			} else {
-				bw.write(previous + "\r\n");
 				// previous line is same as current line
-				if (diff) {
-					// means previous is first of its type
-					diff = false;
-					count++;
-				}
+				bw.write(previous + "\r\n");
 
-				if (count % 1000000 == 0)
+				// means previous is first of its type
+				if (diff)
+					diff = false;
+
+				if (++count % 1000000 == 0)
 					System.out.println("written " + count);
 			}
 			previous = current;
@@ -68,14 +88,17 @@ public class RemoveUniques {
 			prevScan.close();
 			currScan.close();
 		}
-		
-		// at the end, we have the last line not accounted for (since we print previouses)
-		if (!diff)
+
+		// at the end, we have the last line not accounted for (since we print
+		// previouses)
+		if (!diff) {
 			bw.write(previous + "\r\n");
-		else 
-			System.out.println("skipped " + ++skipped);
+			count++;
+		}
 
 		br.close();
 		bw.close();
+
+		System.out.println("Total written: " + count + "\r\nTotal omitted: " + skipped);
 	}
 }
