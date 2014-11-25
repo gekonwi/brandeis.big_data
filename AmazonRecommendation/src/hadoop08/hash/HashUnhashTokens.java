05251d8933856527656c7ccd491bc4da92f800be
recrudescence <calvinmwang@gmail.com>
2014-11-25 02:51:01 -0500

    #62: Added hashing functionality to convert the first x tokens for each line in a file to a integer. The hashing function is as follows: 'tokens[i].hashCode() + (Character.toString(tokens[i].charAt(0)).hashCode())'. The class can be found at hadoop08.hash.HashUnhash, and it takes five parameters: 'inputPath', 'outputPath', '[hash | unhash]', 'HashMapPath', and 'numTokens'.

--- AmazonRecommendation/src/hadoop08/hash/HashUnhashTokens.java
+++ AmazonRecommendation/src/hadoop08/hash/HashUnhashTokens.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 
 /**
  * Hashes or unhashes the values of the first x tokens of a line in a
